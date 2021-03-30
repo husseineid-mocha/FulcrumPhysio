f381c6b014444700c0d97238625fd16ce756c0cb
Hussein Eid <husseineid@mochamacbook.lan>
2021-03-30 18:58:52 -0400

    changes

--- react-app/src/components/Home/index.js
+++ react-app/src/components/Home/index.js
@@ -5,6 +5,7 @@ function Home() {
   return (
     <div>
       <div></div>
+      <div></div>
     </div>
   );
 }
--- react-app/src/components/Navbar/index.js
+++ react-app/src/components/Navbar/index.js
@@ -5,34 +5,54 @@ import { useDispatch, useSelector } from "react-redux";
 import { openLogin, openSignup } from "../../store/modal";
 import "./Navbar.css";
 
-const NavBar = ({ setAuthenticated }) => {
+const NavBar = ({ authenticated, setAuthenticated }) => {
   const dispatch = useDispatch();
+
+  const user = useSelector((state) => state.session);
 
   return (
     <nav>
-      <div>
+      <div className="navbarContainer">
         <div>
           <NavLink to="/" exact={true} activeClassName="active">
             Home
           </NavLink>
         </div>
-        <div>
-          <NavLink to="/login" exact={true} activeClassName="active">
-            <p onClick={() => dispatch(openLogin())}>Log in</p>
-          </NavLink>
-        </div>
-        <div>
-          <NavLink to="/sign-up" exact={true} activeClassName="active">
-            <p onClick={() => dispatch(openSignup())}>Sign up</p>
-          </NavLink>
-        </div>
-        <div>
-          <NavLink to="/users" exact={true} activeClassName="active">
-            Users
-          </NavLink>
-        </div>
-        <div>
-          <LogoutButton setAuthenticated={setAuthenticated} />
+        <div className="loginSignupLogoutContainer">
+          <div>
+            {authenticated === true ? (
+              ""
+            ) : (
+              <NavLink to="/login" exact={true} activeClassName="active">
+                <p onClick={() => dispatch(openLogin())}>Log in</p>
+              </NavLink>
+            )}
+          </div>
+          <div>
+            {authenticated === true ? (
+              ""
+            ) : (
+              <NavLink to="/sign-up" exact={true} activeClassName="active">
+                <p onClick={() => dispatch(openSignup())}>Sign up</p>
+              </NavLink>
+            )}
+          </div>
+          <div>
+            {authenticated === false ? (
+              ""
+            ) : (
+              <NavLink to="/user" exact={true} activeClassName="active">
+                Users
+              </NavLink>
+            )}
+          </div>
+          <div>
+            {authenticated === false ? (
+              ""
+            ) : (
+              <LogoutButton setAuthenticated={setAuthenticated} />
+            )}
+          </div>
         </div>
       </div>
     </nav>
