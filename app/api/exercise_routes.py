6e3d0cacdf4a19c7c4a6590c6fd6490cf86038ef
Hussein Eid <husseineid@mochamacbook.lan>
2021-04-04 18:04:46 -0400

    changes

--- app/api/exercise_routes.py
+++ app/api/exercise_routes.py
@@ -4,7 +4,6 @@ from app.models import db, Category, Exercise, UserExercise, Progress
 exercise_routes = Blueprint('exercises', __name__)
 
 
-
 @exercise_routes.route('/', methods=['PUT'])
 def get_exercises():
     categoryId = request.json
@@ -15,27 +14,28 @@ def get_exercises():
     print(exercises)
     return (exercises)
 
-@exercise_routes.route('/add/<int:id>', method=['POST'])
+
+@exercise_routes.route('/add/<int:id>', methods=['POST'])
 def save_userExercise():
     userExercise = request.json
     new_userExercise = UserExercise(
-        userId = userExercise['userId'],
-        exerciseId = userExercise['exerciseId'])
-        db.session.add(new_userExercise)
-        db.session.commit()
-        return new_userExercise.to_dict()
+        userId=userExercise['userId'],
+        exerciseId=userExercise['exerciseId'])
+    db.session.add(new_userExercise)
+    db.session.commit()
+    return new_userExercise.to_dict()
 
 
-@exercise_routes.route('/<int:id>/add', method=['POST'])
-def save_userExercise():
+@exercise_routes.route('/<int:id>/add', methods=['POST'])
+def save_userProgress():
     progress = request.json
     new_progress = Progress(
-        reps = progress['reps'],
-        sets = progress['sets'],
-        weight = progress['weight'],
-        date = progress['date'],
-        userExerciseId = progress['userExerciseId']
-        )
-        db.session.add(new_progress)
-        db.session.commit()
-        return new_progress.to_dict()    
+        reps=progress['reps'],
+        sets=progress['sets'],
+        weight=progress['weight'],
+        date=progress['date'],
+        userExerciseId=progress['userExerciseId']
+    )
+    db.session.add(new_progress)
+    db.session.commit()
+    return new_progress.to_dict()
