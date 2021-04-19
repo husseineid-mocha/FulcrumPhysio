fdc348225123409ad06e83b699bf02dbb239c378
husseineid-mocha <husseingeid@gmail.com>
2021-04-19 02:43:40 -0400

    features done

--- app/seeds/diagnosisExercise.py
+++ app/seeds/diagnosisExercise.py
@@ -7,27 +7,27 @@ def seed_diagnosisExercise():
     shoulder_instability = DiagnosisExercise(
         diagnosisId=1, exerciseId=4)
     shoulder_instability2 = DiagnosisExercise(
-        diagnosisId=1, exerciseId=7)
+        diagnosisId=1, exerciseId=2)
     mild_subacromial_pain_syndrome = DiagnosisExercise(
-        diagnosisId=2, exerciseId=4)
+        diagnosisId=2, exerciseId=2)
     mild_subacromial_pain_syndrome2 = DiagnosisExercise(
-        diagnosisId=2, exerciseId=5)
+        diagnosisId=2, exerciseId=4)
     moderate_subacromial_pain_syndrome = DiagnosisExercise(
-        diagnosisId=3, exerciseId=5)
+        diagnosisId=3, exerciseId=1)
     moderate_subacromial_pain_syndrome2 = DiagnosisExercise(
-        diagnosisId=3, exerciseId=7)
+        diagnosisId=3, exerciseId=3)
     ac_joint_sprain = DiagnosisExercise(
-        diagnosisId=4, exerciseId=3)
+        diagnosisId=4, exerciseId=5)
     ac_joint_sprain2 = DiagnosisExercise(
-        diagnosisId=4, exerciseId=6)
+        diagnosisId=4, exerciseId=2)
     posterior_shoulder_impingement = DiagnosisExercise(
-        diagnosisId=5, exerciseId=5)
+        diagnosisId=5, exerciseId=3)
     posterior_shoulder_impingement2 = DiagnosisExercise(
-        diagnosisId=5, exerciseId=4)
+        diagnosisId=5, exerciseId=5)
     scapular_winging = DiagnosisExercise(
-        diagnosisId=6, exerciseId=3)
+        diagnosisId=6, exerciseId=4)
     scapular_winging2 = DiagnosisExercise(
-        diagnosisId=6, exerciseId=7)
+        diagnosisId=6, exerciseId=5)
 
     db.session.add(shoulder_instability)
     db.session.add(shoulder_instability2)
@@ -43,37 +43,37 @@ def seed_diagnosisExercise():
     db.session.add(scapular_winging2)
 
     hip_impingement = DiagnosisExercise(
-        diagnosisId=7, exerciseId=4)
+        diagnosisId=7, exerciseId=12)
     hip_impingement2 = DiagnosisExercise(
-        diagnosisId=7, exerciseId=7)
+        diagnosisId=7, exerciseId=11)
     hip_bursitis = DiagnosisExercise(
-        diagnosisId=8, exerciseId=4)
+        diagnosisId=8, exerciseId=11)
     hip_bursitis2 = DiagnosisExercise(
-        diagnosisId=8, exerciseId=5)
+        diagnosisId=8, exerciseId=13)
     hip_labral_tear = DiagnosisExercise(
-        diagnosisId=9, exerciseId=5)
+        diagnosisId=9, exerciseId=9)
     hip_labral_tear2 = DiagnosisExercise(
-        diagnosisId=9, exerciseId=7)
+        diagnosisId=9, exerciseId=12)
     hip_osteoarthritis = DiagnosisExercise(
-        diagnosisId=10, exerciseId=3)
+        diagnosisId=10, exerciseId=8)
     hip_osteoarthritis2 = DiagnosisExercise(
-        diagnosisId=10, exerciseId=6)
+        diagnosisId=10, exerciseId=7)
     itbs = DiagnosisExercise(
-        diagnosisId=11, exerciseId=5)
+        diagnosisId=11, exerciseId=17)
     itbs2 = DiagnosisExercise(
-        diagnosisId=11, exerciseId=4)
+        diagnosisId=11, exerciseId=9)
     piriformis_sx = DiagnosisExercise(
-        diagnosisId=12, exerciseId=3)
+        diagnosisId=12, exerciseId=8)
     piriformis_sx2 = DiagnosisExercise(
-        diagnosisId=12, exerciseId=7)
+        diagnosisId=12, exerciseId=12)
     snapping_hip_sx = DiagnosisExercise(
-        diagnosisId=13, exerciseId=3)
+        diagnosisId=13, exerciseId=14)
     snapping_hip_sx2 = DiagnosisExercise(
-        diagnosisId=13, exerciseId=6)
+        diagnosisId=13, exerciseId=10)
     adductor_strain = DiagnosisExercise(
-        diagnosisId=14, exerciseId=5)
+        diagnosisId=14, exerciseId=12)
     adductor_strain2 = DiagnosisExercise(
-        diagnosisId=14, exerciseId=4)
+        diagnosisId=14, exerciseId=9)
 
     db.session.add(hip_impingement)
     db.session.add(hip_impingement2)
