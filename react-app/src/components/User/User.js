30c1fabd595880df7de5fe5049c74b1c2c53f091
husseineid-mocha <husseingeid@gmail.com>
2021-04-23 00:56:53 -0400

    changes

--- react-app/src/components/User/User.js
+++ react-app/src/components/User/User.js
@@ -117,7 +117,7 @@ function User() {
                 <strong>Email :</strong> {user.email}
               </div>
               <div>
-                <strong>Usernamesss :</strong> {user.username}
+                <strong>Usernames :</strong> {user.username}
               </div>
             </div>
           </div>
